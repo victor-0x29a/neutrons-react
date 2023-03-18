1836dff1d9a829f1afdb7ae4dc0fab2640043ed2
Victor Gomes <cartoonbr495@gmail.com>
2023-03-18 11:41:19 -0400

    add: initial config + dependecies

---
+++ src/routes/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Rotas = () => {
+  return (
+    <Routes>
+      <Route />
+    </Routes>
+  );
+};
+
+export default Routes;
